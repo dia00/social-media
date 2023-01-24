01c793a4fc26845b9d6140338a9b308a9ac0a003
DESKTOP-VF1R09E\lakne <laknerdiana@gmail.com>
2023-01-24 10:49:32 +0200

    profile, login, register

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,5 +1,32 @@
 import './profile.css';
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
-    return
+    return (
+        <>
+            <Topbar/>
+            <div className="profile">
+                <Sidebar/>
+                <div className="profileRight">
+                    <div className="profilerightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImage" src="assets/post/3.jpg" alt="" />
+                            <img className="profileUserImage" src="assets/person/3.png" alt="" />
+                        </div>
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">John Doe</h4>
+                        <p className="profileInfoDesc">This is my description.</p>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed/>
+                        <Rightbar profile/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,23 +3,127 @@ import { FcRating } from "react-icons/fc";
 import { Users } from "../../dummyData.js";
 import Online from "../online/Online";
 
+export default function Rightbar({ profile }) {
+    const Advertisement = () => {
+        return (
+            <img src="/assets/profile-image.jpg" alt="" className="rightbarAd" />
+        )
+    }
 
-export default function Rightbar() {
+    const HomeRightbar = () => {
+        return (
+        <>
+            <div className="birthdayContainer">
+            <FcRating className="birthdayIcon" />
+            <span className="birthdayText">
+                <b>Me</b> and <b>5 other friends</b> have their birthday today
+            </span>
+            </div>
+            <Advertisement/>
+            <h4 className="rightbarTitle">Online Friends</h4>
+            <ul className="rightbarFriendList">
+            {Users.map((u) => (
+                <Online key={u.id} user={u} />
+            ))}
+            </ul>
+        </>
+        );
+    };
+
+    const ProfileRightbar = () => {
+        return (
+        <>
+            <h4 className="rightbarTitle">User Information</h4>
+            <div className="rightbarInfo">
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">City:</span>
+                <span className="rightbarInfoKey">Los Angeles</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">From:</span>
+                <span className="rightbarInfoKey">Paris</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">Relationship:</span>
+                <span className="rightbarInfoKey">In a relationship</span>
+            </div>
+            </div>
+            <h4 className="rightbarTitle">User Friends</h4>
+            <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/6.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/4.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/9.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/10.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/1.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/2.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/7.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            <div className="rightbarFollowing">
+                <img
+                src="assets/person/3.png"
+                alt=""
+                className="rightbarFollowingImage"
+                />
+                <span className="rightbarFollowingName">Agatha Christie</span>
+            </div>
+            </div>
+        </>
+        );
+    };
+
     return (
         <div className="rightbar">
-            <div className="rightbarWrapper">
-                <div className="birthdayContainer">
-                    <FcRating className="birthdayIcon" />
-                    <span className="birthdayText"><b>Me</b> and <b>5 other friends</b> have their birthday today</span>
-                </div>
-                <img src="/assets/profile-image.jpg" alt="" className="rightbarAd" />
-                <h4 className="rightbarTitle">Online Friends</h4>
-                <ul className="rightbarFriendList">
-                    {Users.map((u => (
-                        <Online key={u.id} user={u}/>
-                    )))}
-                </ul>
-            </div>
+        <div className="rightbarWrapper">
+            {profile ? <ProfileRightbar/> : <HomeRightbar/>}
         </div>
-    )
+        </div>
+    );
 }
